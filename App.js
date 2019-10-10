49e4258a89e6592d0998dba767eca05463b08fad
Aman Mittal <amandeepmittal@live.com>
2019-10-10 14:27:41 +0530

    refactor

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  StatusBar
+} from 'react-native'
 import * as tf from '@tensorflow/tfjs'
 import '@tensorflow/tfjs-react-native'
 import * as mobilenet from '@tensorflow-models/mobilenet'
@@ -14,22 +20,28 @@ class App extends React.Component {
     this.setState({
       isTfReady: true
     })
-    this.model = await mobilenet.load()
-    this.setState({ isModelReady: true })
+    // this.model = await mobilenet.load()
+    // this.setState({ isModelReady: true })
   }
   render() {
+    const { isTfReady, isModelReady } = this.state
     return (
       <View style={styles.container}>
-        <Text>TFJS ready? {this.state.isTfReady ? <Text>Yes</Text> : ''}</Text>
+        <StatusBar barStyle='light-content' />
+        <View style={styles.loadingContainer}>
+          <Text style={styles.commonTextStyles}>
+            TFJS ready? {isTfReady ? <Text>✅</Text> : ''}
+          </Text>
 
-        <Text>
-          Model ready?{' '}
-          {this.state.isModelReady ? (
-            <Text>Yes</Text>
-          ) : (
-            <Text>Loading Model...</Text>
-          )}
-        </Text>
+          <View style={styles.loadingModelContainer}>
+            <Text style={styles.commonTextStyles}>Model ready? </Text>
+            {isTfReady ? (
+              <Text style={styles.commonTextStyles}>✅</Text>
+            ) : (
+              <ActivityIndicator size='small' />
+            )}
+          </View>
+        </View>
       </View>
     )
   }
@@ -38,9 +50,20 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#171f24'
+  },
+  loadingContainer: {
+    marginTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  commonTextStyles: {
+    color: 'white',
+    fontSize: 16
+  },
+  loadingModelContainer: {
+    flexDirection: 'row',
+    marginTop: 10
   }
 })
 
